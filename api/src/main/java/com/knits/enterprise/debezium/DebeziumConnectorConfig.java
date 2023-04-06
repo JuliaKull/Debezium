73513cc0c35ef226633a1cea1991976ab9df9260
JuliaKull <7102777@mail.ru>
2023-04-06 19:09:43 +0300

    solved issue with wal_level

--- api/src/main/java/com/knits/enterprise/debezium/DebeziumConnectorConfig.java
+++ api/src/main/java/com/knits/enterprise/debezium/DebeziumConnectorConfig.java
@@ -14,16 +14,17 @@ public class DebeziumConnectorConfig {
         return io.debezium.config.Configuration.create()
                 .with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
                 .with("offset.storage",  "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", "/path/cdc/offset/student-offset.dat")
+                .with("offset.storage.file.filename", "/path/cdc/offset/employee-offset.dat")
                 .with("offset.flush.interval.ms", 60000)
-                .with("name", "student-postgres-connector")
+                .with("name", "postgres-connector")
                 .with("database.server.name", "employee")
                 .with("database.hostname", "localhost")
                 .with("database.port", "5432")
                 .with("database.user", "postgres")
                 .with("database.password", System.getenv("POSTGRESQL_PASS"))
-                .with("topic.prefix", "my-app-connector")
-                .with("database.dbname", "Debezium")
+                .with("topic.prefix", "debezium-connector")
+                .with("database.dbname", "postgres")
+                .with("plugin.name", "pgoutput")
                 .with("table.whitelist", "public.employee").build();
     }
 
