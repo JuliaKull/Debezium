26d0fc7ef6ef51891421cf2690d9c9810bdc3636
JuliaKull <7102777@mail.ru>
2023-04-10 13:44:44 +0300

    updated Listeren

--- api/src/main/java/com/knits/enterprise/debezium/DebeziumListener.java
+++ api/src/main/java/com/knits/enterprise/debezium/DebeziumListener.java
@@ -2,7 +2,11 @@ package com.knits.enterprise.debezium;
 
 import com.knits.enterprise.service.company.EmployeeService;
 import io.debezium.config.Configuration;
+import io.debezium.embedded.Connect;
 import io.debezium.embedded.EmbeddedEngine;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.engine.RecordChangeEvent;
+import io.debezium.engine.format.ChangeEventFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.connect.data.Field;
@@ -30,46 +34,40 @@ public class DebeziumListener {
 
     private final EmployeeService employeeService;
 
-    private final EmbeddedEngine debeziumEngine;
+    private final DebeziumEngine<RecordChangeEvent<SourceRecord>> debeziumEngine;
 
-    public DebeziumListener(Configuration employeeConnectorConfiguration, EmployeeService employeeService) {
+    public DebeziumListener(Configuration customerConnectorConfiguration, EmployeeService employeeService) {
 
-        this.debeziumEngine = EmbeddedEngine
-                .create()
-                .using(employeeConnectorConfiguration)
+        this.debeziumEngine = DebeziumEngine.create(ChangeEventFormat.of(Connect.class))
+                .using(customerConnectorConfiguration.asProperties())
                 .notifying(this::handleChangeEvent)
                 .build();
 
         this.employeeService = employeeService;
     }
 
-    private void handleChangeEvent(SourceRecord sourceRecord) {
-        Struct sourceRecordValue = (Struct) sourceRecord.value();
+    private void handleChangeEvent(RecordChangeEvent<SourceRecord> sourceRecordRecordChangeEvent) {
+        SourceRecord sourceRecord = sourceRecordRecordChangeEvent.record();
 
-        if (sourceRecordValue != null) {
-            Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
+        log.info("Key = '" + sourceRecord.key() + "' value = '" + sourceRecord.value() + "'");
 
-            //Only if this is a transactional operation.
-            if (operation != Operation.READ) {
+        Struct sourceRecordChangeValue= (Struct) sourceRecord.value();
 
-                Map<String, Object> message;
-                String record = AFTER; //For Update & Insert operations.
+        if (sourceRecordChangeValue != null) {
+            Operation operation = Operation.forCode((String) sourceRecordChangeValue.get(OPERATION));
 
-                if (operation == Operation.DELETE) {
-                    record = BEFORE; //For Delete operations.
-                }
+            if(operation != Operation.READ) {
+                String record = operation == Operation.DELETE ? BEFORE : AFTER; // Handling Update & Insert operations.
 
-                //Build a map with all row data received.
-                Struct struct = (Struct) sourceRecordValue.get(record);
-                message = struct.schema().fields().stream()
+                Struct struct = (Struct) sourceRecordChangeValue.get(record);
+                Map<String, Object> payload = struct.schema().fields().stream()
                         .map(Field::name)
                         .filter(fieldName -> struct.get(fieldName) != null)
                         .map(fieldName -> Pair.of(fieldName, struct.get(fieldName)))
                         .collect(toMap(Pair::getKey, Pair::getValue));
 
-                //Call the service to handle the data change.
-                this.employeeService.replicateData(message, operation);
-                log.info("Data Changed: {} with Operation: {}", message, operation.name());
+                this.employeeService.replicateData(payload, operation);
+                log.info("Updated Data: {} with Operation: {}", payload, operation.name());
             }
         }
     }
